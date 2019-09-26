e1aad02533edec41af24e55e0ab71b0c125109d8
Tim Morse <sasessiontc@gmail.com>
2019-09-26 11:06:06 -0400

    Initial Commit

---
+++ PactSafe iOS DemoTests/PactSafe_iOS_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  PactSafe_iOS_DemoTests.swift
+//  PactSafe iOS DemoTests
+//
+//  Created by Tim Morse  on 9/26/19.
+//  Copyright © 2019 Tim Morse . All rights reserved.
+//
+
+import XCTest
+@testable import PactSafe_iOS_Demo
+
+class PactSafe_iOS_DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
